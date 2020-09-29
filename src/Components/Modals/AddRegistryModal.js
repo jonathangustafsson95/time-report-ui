2b62ba93cc4a8669f9018a91f3ef4be773e6cf25
Joel Askling <joel.askling@welixo.com>
2020-09-29 16:54:04 +0200

    La till en meny under AddRegistry.

--- src/Components/Modals/AddRegistryModal.js
+++ src/Components/Modals/AddRegistryModal.js
@@ -7,8 +7,16 @@ import { addRegistryToStore } from "../../Redux/Actions/RegistryActions";
 const AddRegistry = ({ addRegistry, date, modalIsOpen, onCloseModal }) => {
   const [hours, setHours] = useState(1);
   const [minutes, setMinutes] = useState(0);
+  const [registryType, setRegistryType] = useState({
+    internal: true,
+    customer: false,
+  });
 
-  console.log(date);
+  const toggleButton = (buttonName) => {
+    setRegistryType({
+      [buttonName]: !registryType[buttonName],
+    });
+  };
 
   const onAddRegistry = () => {
     const mins = (parseFloat(minutes) / 60) * 1;
@@ -37,11 +45,108 @@ const AddRegistry = ({ addRegistry, date, modalIsOpen, onCloseModal }) => {
   };
 
   return (
-    <Modal show={modalIsOpen} onHide={onCloseModal}>
-      {/* <Modal.Header>
-        <Text>New Report</Text>
-      </Modal.Header> */}
+    <RegistryModal show={modalIsOpen} onHide={onCloseModal}>
       <Modal.Body>
+        <Text>New Registry</Text>
+        <MenuDiv>
+          <Title>Internal Time</Title>
+          <SpaceDiv />
+          <Title>Customer Time</Title>
+        </MenuDiv>
+
+        <IconMenuDiv>
+          <IconButton
+            selected={registryType.internal}
+            onClick={() => toggleButton("internal")}
+          >
+            <Icon
+              xmlns="http://www.w3.org/2000/svg"
+              width="17"
+              height="17"
+              viewBox="0 0 17 17"
+            >
+              <g
+                id="Group_285"
+                data-name="Group 285"
+                transform="translate(-211 -22.485)"
+              >
+                <g
+                  id="Ellipse_94"
+                  data-name="Ellipse 94"
+                  transform="translate(211 22.485)"
+                  fill="none"
+                  stroke="#707070"
+                  stroke-width="1"
+                  opacity="0.6"
+                >
+                  <circle cx="8.5" cy="8.5" r="8.5" stroke="none" />
+                  <circle cx="8.5" cy="8.5" r="8" fill="none" />
+                </g>
+                <g
+                  id="Ellipse_97"
+                  data-name="Ellipse 97"
+                  transform="translate(217 28.485)"
+                  fill="#707070"
+                  stroke="#707070"
+                  stroke-width="1"
+                  opacity="0.6"
+                >
+                  <circle cx="2.5" cy="2.5" r="2.5" stroke="none" />
+                  <circle cx="2.5" cy="2.5" r="2" fill="none" />
+                </g>
+              </g>
+            </Icon>
+          </IconButton>
+          <MenuLine />
+          <IconButton
+            selected={registryType.customer}
+            onClick={() => toggleButton("customer")}
+          >
+            <Icon
+              xmlns="http://www.w3.org/2000/svg"
+              width="17"
+              height="17"
+              viewBox="0 0 17 17"
+            >
+              <g
+                id="Group_285"
+                data-name="Group 285"
+                transform="translate(-211 -22.485)"
+              >
+                <g
+                  id="Ellipse_94"
+                  data-name="Ellipse 94"
+                  transform="translate(211 22.485)"
+                  fill="none"
+                  stroke="#707070"
+                  stroke-width="1"
+                  opacity="0.6"
+                >
+                  <circle cx="8.5" cy="8.5" r="8.5" stroke="none" />
+                  <circle cx="8.5" cy="8.5" r="8" fill="none" />
+                </g>
+                <g
+                  id="Ellipse_97"
+                  data-name="Ellipse 97"
+                  transform="translate(217 28.485)"
+                  fill="#707070"
+                  stroke="#707070"
+                  stroke-width="1"
+                  opacity="0.6"
+                >
+                  <circle cx="2.5" cy="2.5" r="2.5" stroke="none" />
+                  <circle cx="2.5" cy="2.5" r="2" fill="none" />
+                </g>
+              </g>
+            </Icon>
+          </IconButton>
+        </IconMenuDiv>
+        <MenuDiv>
+          <SecondTitle>H</SecondTitle>
+          <SecondSpaceDiv />
+          <SecondTitle>M</SecondTitle>
+        </MenuDiv>
+
         <InputDiv>
           <Input
             type="number"
@@ -59,9 +164,85 @@ const AddRegistry = ({ addRegistry, date, modalIsOpen, onCloseModal }) => {
         </InputDiv>
         <Button onClick={onAddRegistry}>Add</Button>
       </Modal.Body>
-    </Modal>
+    </RegistryModal>
   );
 };
+
+const IconButton = styled.button`
+  padding: 0;
+  border: none;
+  background: none;
+  &:focus {
+    outline: none;
+  }
+  svg {
+    g {
+      stroke: ${(props) => (props.selected ? "#F00A6B" : "#707070")};
+      opacity: ${(props) => (props.selected ? "1" : "0.8")};
+    }
+  }
+`;
+
+const Text = styled.p`
+  font-family: Roboto;
+  font-weight: normal;
+  font-size: 16px;
+  letter-spacing: 0.08em;
+  line-height: 46px;
+  text-align: left;
+  color: #585656;
+  margin: 0;
+  text-align: center;
+`;
+
+const SpaceDiv = styled.div`
+  width: 80px;
+  margin-right: 30px;
+`;
+
+const SecondSpaceDiv = styled.div`
+  width: 20px;
+`;
+
+const Title = styled(Text)`
+  font-size: 12px;
+`;
+
+const SecondTitle = styled(Text)`
+  opacity: 0.7;
+`;
+
+const MenuDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconMenuDiv = styled(MenuDiv)`
+  margin-bottom: 30px;
+`;
+
+const MenuLine = styled.hr`
+  margin: 0;
+  width: 162px;
+  margin-top: 1px;
+  background: transparent;
+  border: 2px solid #707070;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.25);
+  border-right: 1px solid rgba(255, 255, 255, 0.25);
+  border-left: 1px solid rgba(255, 255, 255, 0.25);
+  opacity: 0.6;
+  margin-right: 7px;
+  margin-left: 7px;
+`;
+
+const Icon = styled.svg`
+  transform: scale(1.4);
+`;
+
+const RegistryModal = styled(Modal)`
+  margin-top: 15%;
+`;
 
 const Button = styled.button`
   font-family: Roboto;
@@ -76,25 +257,11 @@ const Button = styled.button`
   margin-left: 30%;
 `;
 
-const Text = styled.p`
-  font-family: Roboto;
-  font-weight: normal;
-  font-size: 14px;
-  letter-spacing: 0.08em;
-  line-height: 46px;
-  text-align: left;
-  color: #585656;
-  opacity: 0.7;
-  margin: 0;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
 const Line = styled.hr`
   margin: 0;
   width: 0px;
   height: 24px;
-  background: transparent;
+  background-color: #585656;
   border: 1px solid #585656;
   opacity: 0.7;
 `;
@@ -110,8 +277,8 @@ const InputDiv = styled.div`
   background: #fff;
   opacity: 0.7;
   border: 2px solid #585656;
-  margin-bottom: 50px;
-  margin-top: 30px;
+  margin-top: -13px;
+  margin-bottom: 30px;
 `;
 
 const Input = styled.input`
