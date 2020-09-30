412dd8408e2a4c0aa1920a462e5897dae920a11e
Joel Askling <joel.askling@welixo.com>
2020-09-30 16:01:43 +0200

    fixat authorize

--- src/Components/SignInComponents/SignInComponent.js
+++ src/Components/SignInComponents/SignInComponent.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import styled from "styled-components";
 import { connect } from "react-redux";
 import { authorize } from "../../Redux/Actions/AuthActions";
 
@@ -15,32 +14,45 @@ const SignIn = ({ authData, signIn }) => {
       userName: userName,
       password: password,
     });
-    console.log("OnSignIn");
   };
 
   return (
-    <Form onSubmit={onSignIn}>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          placeholder="Enter username"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-      </Form.Group>
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder="username"
+      ></input>
+      <input
+        type="text"
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      ></input>
+      <button onClick={onSignIn}>sign in</button>
+    </div>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-      </Form.Group>
+    // <Form onSubmit={() => onSignIn}>
+    //   <Form.Group controlId="formBasicEmail">
+    //     <Form.Label>Username</Form.Label>
+    //     <Form.Control
+    //       placeholder="Enter username"
+    //       onChange={(e) => setUserName(e.target.value)}
+    //     />
+    //   </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Sign in
-      </Button>
-    </Form>
+    //   <Form.Group controlId="formBasicPassword">
+    //     <Form.Label>Password</Form.Label>
+    //     <Form.Control
+    //       type="password"
+    //       placeholder="Password"
+    //       onChange={(e) => setPassword(e.target.value)}
+    //     />
+    //   </Form.Group>
+
+    //   <Button variant="primary" type="submit">
+    //     Sign in
+    //   </Button>
+    // </Form>
   );
 };
 
@@ -52,7 +64,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    signIn: () => dispatch(authorize()),
+    signIn: (user) => dispatch(authorize(user)),
   };
 };
 
--- src/Redux/Actions/AuthActions.js
+++ src/Redux/Actions/AuthActions.js
@@ -1,37 +1,43 @@
-import * as Types from "../Types/RegistryTypes";
+import * as Types from "../Types/AuthTypes";
 import axios from "axios";
 import * as service from "../ApiService/Service";
 
 export const authorizeRequest = () => {
-    return {
-        type: Types.FETCH_REGISTRIES_BY_WEEK_REQUEST,
-      };
-}
+  return {
+    type: Types.AUTHORIZATION_REQUEST,
+  };
+};
 
 export const authorizeSuccess = (token) => {
-    return {
-        type: Types.FETCH_REGISTRIES_BY_WEEK_REQUEST,
-        payload: token
-      };
-}
+  return {
+    type: Types.AUTHORIZATION_SUCCESS,
+    payload: token,
+  };
+};
 export const authorizeFailure = (error) => {
-    return {
-        type: Types.FETCH_REGISTRIES_BY_WEEK_REQUEST,
-        payload: error
-      };
-}
+  return {
+    type: Types.AUTHORIZATION_FAILURE,
+    payload: error,
+  };
+};
 
 export const authorize = (userData) => {
-    return (dispatch) => {
-        dispatch(authorizeRequest())
-        axios.post({
-            url: service.baseUrl + "/system/login",
-            method: "post",
-            data: userData,
-        }).then((response) => {
-            dispatch(authorizeSuccess(response))
-        }).catch((errorMsg) => {
-            dispatch(authorizeFailure(errorMsg))
-        })
-    }
-}
+  console.log(userData);
+  return (dispatch) => {
+    dispatch(authorizeRequest());
+    let payload = {
+      user: userData,
+    };
+    axios({
+      url: service.baseUrl + "/system/login",
+      method: "post",
+      data: payload,
+    })
+      .then((response) => {
+        dispatch(authorizeSuccess(response));
+      })
+      .catch((error) => {
+        dispatch(authorizeFailure(error));
+      });
+  };
+};
