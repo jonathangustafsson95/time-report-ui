724abc163b32ef62d1c193577901668d88bbc371
Joel Askling <joel.askling@welixo.com>
2020-10-01 11:54:00 +0200

    tog bort authenticationActions

--- src/Redux/Actions/AuthenticationActions.js
+++
@@ -1,9 +0,0 @@
-import * as Types from "../Types/RegistryTypes";
-import axios from "axios";
-import * as service from "../ApiService/Service";
-
-export const userActions = {
-    login,
-    logout,
-    getAll
-};
