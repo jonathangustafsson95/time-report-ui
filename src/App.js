c46346cced3a4e18b5301e0a6c53c938b48035e5
Joel Askling <joel.askling@welixo.com>
2020-10-01 11:50:41 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import Layout from "./Components/Layout";
 import SignIn from "./Components/SignInComponents/SignInComponent";
 import { connect } from "react-redux";
-import {isMobile} from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 
 function App({ authData }) {
   if (!authData.user) {
-      if (isMobile) {
-        return(
-          <div>
-        <h1>Det här skrivs ut på mobilversionen</h1>
-        <SignIn />
-      </div>
-        )
-      }
+    if (isMobile) {
       return (
+        <div>
+          <h1>Det här skrivs ut på mobilversionen</h1>
+          <SignIn />
+        </div>
+      );
+    }
+    return (
       <div>
         <SignIn />
       </div>
