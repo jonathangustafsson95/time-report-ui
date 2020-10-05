a72f0cda24e6ad3aaffbaa841d04599786dbfb8f
Joel Askling <joel.askling@welixo.com>
2020-10-05 09:41:43 +0200

    Tagit bort userData från redux

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import userReducer from "./Reducers/UserReducer";
 import registryReducer from "./Reducers/RegistryReducer";
 import authReducer from "./Reducers/AuthReducer";
 
 const rootReducer = combineReducers({
-  userData: userReducer,
   registryData: registryReducer,
   authData: authReducer,
 });
