7e728dff8be6d3694a57a57ed1f1bfab828455df
Tamara S. A. Malik <tamara.m2012@gmail.com>
2020-10-08 15:20:15 +0200

    backButton

--- src/Components/ProjectsComponents/ProjectDetailComponent.js
+++ src/Components/ProjectsComponents/ProjectDetailComponent.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'
 import styled from 'styled-components';
+import { NavLink } from "react-router-dom";
 import IconArrow from "./Images/Group247.png"
 
-const ProjectDetail = () => {
-    return ( 
+const ProjectDetailComponent = ({markedMissions}) => {
+  console.log(markedMissions);
+  return ( 
               <div>
                 <BackDiv className="align-baseline">
+                  <LinkText to="/projects">
                   <Button variant="light" color="#f00a6b">
                   <img src={IconArrow}></img>
                     Back
                   </Button> 
+                  </LinkText>
                 </BackDiv>
                 <text> Mission Details Here </text>
+             
               </div> 
        
           );
 }
+
+
  const BackDiv=styled.div`
  display: flex;
   flex-direction: row;
@@ -26,4 +32,12 @@ const ProjectDetail = () => {
   margin-top: 60px;
  
  `
-export default ProjectDetail;
+const LinkText= styled(NavLink)`
+font-family: Roboto;
+font-weight: normal;
+font-size: 20px;
+letter-spacing: 0.02em;
+text-align: left;
+color: #302f2f;
+`
+export default ProjectDetailComponent;
