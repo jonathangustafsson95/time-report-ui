43889ddee8d5daa80b1306f0e727b5a7f8a94b36
Tamara S. A. Malik <tamara.m2012@gmail.com>
2020-10-08 08:12:12 +0200

    FavoriteMissionsAndLink

---
+++ src/Components/ProjectsComponents/ProjectDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ProjectDetail = () => {
+    return ( 
+                
+            <text> Mission Details Here </text>
+       
+          );
+}
+ 
+export default ProjectDetail;
