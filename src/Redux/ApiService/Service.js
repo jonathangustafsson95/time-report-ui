21fe872269551765f2a90e952808c1d5adf75c3c
challe <im.challe@gmail.com>
2020-11-06 10:00:49 +0100

    API service

--- src/Redux/ApiService/Service.js
+++ src/Redux/ApiService/Service.js
@@ -1,2 +1,3 @@
-export const baseUrl = "https://193.10.247.45/bulbasaur/api";
-//export const baseUrl = "https://localhost:5001/api";
+//export const baseUrl = "https://193.10.247.45/bulbasaur/api";
+//export const baseUrl = "https://localhost:44362/api";
+export const baseUrl = "https://localhost:5001/api";
